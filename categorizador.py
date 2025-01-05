3617ae7d8eef672c6fef397adcb5ba5346e32509
feliipecardosoo <fglibino@gmail.com>
2025-01-04 23:45:33 -0300

    att read

---
+++ categorizador.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "system",
+      "content": """
+        Classifique o produto abaixo em uma das categorias: Higiene Pessoal, Moda ou casa e de uma descrição da categoria.
+       """
+    },
+    {
+        "role": "user",
+        "content": """
+        Escova de dentes de bambu
+        """
+    }
+  ],
+  response_format={
+    "type": "text"
+  },
+  temperature=1,
+  max_completion_tokens=2048,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(response.choices[0].message.content)
